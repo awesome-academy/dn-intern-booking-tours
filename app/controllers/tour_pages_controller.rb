7b9e06f533c24afee7779c2f8736412915666b68
hoanvn1999 <hoanvn1999@gmail.com>
2020-12-15 08:20:19 +0700

    Admin - Manage tours - Show tours

--- app/controllers/tour_pages_controller.rb
+++ app/controllers/tour_pages_controller.rb
@@ -1,7 +1,5 @@
 class TourPagesController < ApplicationController
   def home
-    redirect_to admin_bookings_path if logged_in? && current_admin?
-
     @tours = Tour.sort_place_name
                  .paginate(page: params[:page],
                            per_page: Settings.per_page.default)
